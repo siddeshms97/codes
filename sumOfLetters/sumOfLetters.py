e937d0311dcd9e88873ac277c27fdf516ec634bc
siddeshms <siddeshms18@gmail.com>
2021-10-13 14:51:27 +0530

    added sumOfLetters.py with question on top

--- sumOfLetters/sumOfLetters.py
+++ sumOfLetters/sumOfLetters.py
@@ -1,3 +1,16 @@
+'''
+Sum of Letters
+
+During a class assessment, a teacher asks students to solve the following
+problem IfA=0,B=1.C=B+A, D=C+B, and so on, find the sum of the alphabets of a given word
+
+Input Specification:
+input1: String representing any word
+
+Output Specification:
+Return an integer value which represents the sum of all the alphabets in input1, as per the 
+above-given scenario
+'''
 def SOL(str):
     letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     values = [0,1]
